1a705e0ceb86c591b51c4d529b5ad75231b30480
Simon Shields <simon@lineageos.org>
2018-07-08 00:58:31 +1000

    Revert "usb: dwc2: Enable LPM"
    This reverts commit 7455f8b7f0b3f3409f50e52ae6fd23fbd1a611dd.

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -4671,9 +4671,6 @@ int dwc2_gadget_init(struct dwc2_hsotg *hsotg)
 	hsotg->gadget.name = dev_name(dev);
 	hsotg->remote_wakeup_allowed = 0;
 
-	if (hsotg->params.lpm)
-		hsotg->gadget.lpm_capable = true;
-
 	if (hsotg->dr_mode == USB_DR_MODE_OTG)
 		hsotg->gadget.is_otg = 1;
 	else if (hsotg->dr_mode == USB_DR_MODE_PERIPHERAL)
